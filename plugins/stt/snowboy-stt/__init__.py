2fc4435895d98e7ddd556312e190879214fc5d56
KrawallKurt <krawallkurt@infokl.duckdns.org>
2016-08-07 01:04:43 +0200

    initial snowboy functionality
    First working version of snowboy. For passive stt only

--- plugins/stt/snowboy-stt/__init__.py
+++ plugins/stt/snowboy-stt/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from .snowboy import snowboydecoder
+from .snowboy import SnowboySTTPlugin
--- plugins/stt/snowboy-stt/snowboy.py
+++ plugins/stt/snowboy-stt/snowboy.py
@@ -1,114 +1,41 @@
-import logging
-import requests
+import snowboydetect
 from jasper import plugin
 
-# There seems to be no way to get language setting of the defined app
-# Last updated: April 06, 2016
-SUPPORTED_LANG = (
-    'de',
-    'en',
-    'es',
-    'et',
-    'fr',
-    'it',
-    'nl',
-    'pl',
-    'pt',
-    'ru',
-    'sv'
-)
-
-
-class WitAiSTTPlugin(plugin.STTPlugin):
+class SnowboySTTPlugin(plugin.STTPlugin):
     """
-    Speech-To-Text implementation which relies on the Wit.ai Speech API.
+    Hotword Detection which relies on Snowboy.
 
-    This implementation requires an Wit.ai Access Token to be present in
-    profile.yml. Please sign up at https://wit.ai and copy your instance
-    token, which can be found under Settings in the Wit console to your
-    profile.yml:
+    Excerpt from sample profile.yml:
+
         ...
-        stt_engine: witai-stt
-        witai-stt:
-          access_token:    ERJKGE86SOMERANDOMTOKEN23471AB
+        snowboy-stt:
+            model: Hello_Jasper.pmdl
+
     """
 
     def __init__(self, *args, **kwargs):
-        """
-        Create Plugin Instance
-        """
         plugin.STTPlugin.__init__(self, *args, **kwargs)
-        self._logger = logging.getLogger(__name__)
-        self.token = self.profile['witai-stt']['access_token']
 
+        self.resource_file = "plugins/stt/snowboy-stt/common.res"
+        self.model = self.profile['snowboy']['model']
         try:
-            language = self.profile['language']
+            self.sensitivity = self.profile['snowboy']['sensitivity']
         except KeyError:
-            language = 'en-US'
-        if language.split('-')[0] not in SUPPORTED_LANG:
-            raise ValueError('Language %s is not supported.',
-                             language.split('-')[0])
+            self.sensitivity = "0.5"
 
-    @property
-    def token(self):
-        """
-        Return defined acess token.
-        """
-        return self._token
-
-    @property
-    def language(self):
-        """
-        Returns selected language
-        """
-        return self._language
-
-    @token.setter
-    def token(self, value):
-        """
-        Sets property token
-        """
-        self._token = value
-        self._headers = {'Authorization': 'Bearer %s' % self.token,
-                         'accept': 'application/json',
-                         'Content-Type': 'audio/wav'}
-
-    @property
-    def headers(self):
-        """
-        Return headers
-        """
-        return self._headers
+        self.detector = snowboydetect.SnowboyDetect(
+            resource_filename=self.resource_file,
+            model_str=self.model)
+        self.detector.SetAudioGain(1)
+        self.detector.SetSensitivity(self.sensitivity);
 
     def transcribe(self, fp):
-        """
-        transcribes given audio file by uploading to wit.ai and returning
-        received text from json answer.
-        """
+        fp.seek(44)
         data = fp.read()
-        r = requests.post('https://api.wit.ai/speech?v=20150101',
-                          data=data,
-                          headers=self.headers)
-        try:
-            r.raise_for_status()
-            text = r.json()['_text']
-        except requests.exceptions.HTTPError:
-            self._logger.critical('Request failed with response: %r',
-                                  r.text,
-                                  exc_info=True)
-            return []
-        except requests.exceptions.RequestException:
-            self._logger.critical('Request failed.', exc_info=True)
-            return []
-        except ValueError as e:
-            self._logger.critical('Cannot parse response: %s',
-                                  e.args[0])
-            return []
-        except KeyError:
-            self._logger.critical('Cannot parse response.',
-                                  exc_info=True)
-            return []
+
+        ans = self.detector.RunDetection(data)
+
+        if ans:
+            return [self._vocabulary_phrases[-1]]
         else:
-            transcribed = [text.upper()]
-            self._logger.info('Transcribed: %r', transcribed)
-            return transcribed
+            return []
