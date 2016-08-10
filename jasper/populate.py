197bb17f3b30be103229a5a09bd933922d509016
chrobione <chrobione@gmail.com>
2016-08-10 02:57:12 -0600

    fixed pep8 issue

--- jasper/populate.py
+++ jasper/populate.py
@@ -25,7 +25,7 @@ def run():
     # name
     simple_request('first_name', 'First name')
     simple_request('last_name', 'Last name')
-    simple_request('keyword','What is my Name?')
+    simple_request('keyword', 'What is my Name?')
 
     # gmail
     print("\nJasper uses your Gmail to send notifications. Alternatively, " +
