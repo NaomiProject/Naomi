3dc040144b0ed11991dc98110a0c507ffd18ad90
G10DRAS <olxboss@gmail.com>
2016-08-24 19:56:21 +0530

    Update g2p.py
    Generated G2P Dictionary will be in alphabetically sorted order

--- plugins/stt/pocketsphinx-stt/g2p.py
+++ plugins/stt/pocketsphinx-stt/g2p.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 import tempfile
 import logging
+import collections
 from . import phonemeconversion
 
 
@@ -75,7 +76,9 @@ def execute(executable, version, fst_model, input, is_file=False, nbest=None):
             if word not in result:
                 result[word] = []
             result[word].append(pronounciation)
-    return result
+            
+    results = collections.OrderedDict(sorted(result.items()))
+    return results
 
 
 class PhonetisaurusG2P(object):
