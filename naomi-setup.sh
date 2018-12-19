fcf91da69eae733ae4f06ae27faf0bec4c481f94
Aaron Chantrill <aaron.chantrill@dottywood.org>
2018-12-19 18:14:52 +0000

    Add gettext to naomi-setup.sh
    This is a few additional tweaks to naomi-setup.sh. First, I
    updated the header to reflect that python would be installed
    to the ~/.naomi/local/bin directory and not to the ~/local/bin
    directory. That was a change I made just to keep all the
    naomi support files together in one location and make it clear
    what they are for.
    
    Then I changed the instructions about needing to install
    libssl1.0-dev to libssl-dev just to match the actual library
    that seems to work.
    
    I added gettext to the list of apt modules being installed so
    that the system is ready to run "compile_translations.sh" later,
    and added some text explaining to the user what is being done
    in case they receive a sudo password prompt.
    
    Added a line telling the user to use the "./Naomi" command instead
    of "./Naomi.py" to start Naomi. Added --repopulate to the ./Naomi
    command to force repopulation of the profile even if one already
    exists.

--- naomi-setup.sh
+++ naomi-setup.sh
@@ -3,24 +3,25 @@
 #########################################
 # Installing python and necessary packages
 # for Naomi. This script will install python
-# into the ~/local/bin directory and install
-# naomi & requirements in their respective
-# directories.
+# into the ~/.naomi/local/bin directory and
+# install naomi & requirements in their
+# respective directories.
 #########################################
 # Assume this program is in the main Naomi directory
 # so we can save and return to it.
 export NAOMI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "NAOMI_DIR = $NAOMI_DIR"
+echo "Updating apt, preparing to install libssl-dev, gettext, portaudio19-dev and libasound2-dev"
 # install dependencies
 sudo apt-get update
-# libssl1.0-dev required to get the python _ssl module working
-sudo apt-get install libssl-dev portaudio19-dev libasound2-dev -y
+# libssl-dev required to get the python _ssl module working
+sudo apt-get install libssl-dev gettext portaudio19-dev libasound2-dev -y
 
 # installing python 2.7.13
 echo 'Installing python 2.7.13 to ~/.naomi/local'
 mkdir -p ~/.naomi/local
 cd ~/.naomi/local
-wget http://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
+wget https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
 tar xvzf Python-2.7.13.tgz
 cd Python-2.7.13
 ./configure
@@ -28,13 +29,15 @@ make
 make altinstall prefix=~/.naomi/local  # specify local installation directory
 ln -s ~/.naomi/local/bin/python2.7 ~/.naomi/local/bin/python
 cd ..  # ~/.naomi/local
+
 # install setuptools and pip for package management
 echo 'Installing setuptools'
-wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz#md5=7df2a529a074f613b509fb44feefe74e
+wget https://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz#md5=7df2a529a074f613b509fb44feefe74e
 tar xvzf setuptools-0.6c11.tar.gz
 cd setuptools-0.6c11 # ~/.naomi/local/setuptools-0.6c11
 ~/.naomi/local/bin/python setup.py install  # specify the path to the python you installed above
 cd .. # ~/.naomi/local
+
 # The old version of pip expects to access pypi.org using http. PyPi now
 # requires ssl for all connections.
 wget https://files.pythonhosted.org/packages/45/ae/8a0ad77defb7cc903f09e551d88b443304a9bd6e6f124e75c0fbbf6de8f7/pip-18.1.tar.gz
@@ -52,5 +55,6 @@ cd $NAOMI_DIR
 echo "#!/bin/bash" > Naomi
 echo "~/.naomi/local/bin/python $NAOMI_DIR/Naomi.py $@" >> Naomi
 chmod a+x Naomi
-./Naomi
+echo "In the future, run $NAOMI_DIR/Naomi to start Naomi"
+./Naomi --repopulate
 
