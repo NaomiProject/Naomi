3512dc5ede00c6a538e0747b220440b0ce73d580
Aaron Chantrill <aaron.chantrill@dottywood.org>
2018-12-29 16:49:08 -0500

    Simplifying run_command
    No need to create a variable in run_command.

--- naomi/run_command.py
+++ naomi/run_command.py
@@ -2,8 +2,7 @@
 import subprocess
 
 def run_command(command):
-    output = subprocess.check_output(
+    return subprocess.check_output(
         command,
         shell=False
     ).decode('utf-8').strip()
-    return output
