74835934ffaa314d25cfb2968560159331fb4bc4
Aaron Chantrill <aaron.chantrill@dottywood.org>
2018-12-29 16:56:15 -0500

    Codacy and subprocess
    Honestly running out of things to try here.

--- naomi/run_command.py
+++ naomi/run_command.py
@@ -2,7 +2,4 @@
 import subprocess
 
 def run_command(command):
-    return subprocess.check_output(
-        command,
-        shell=False
-    ).decode('utf-8').strip()
+    return subprocess.check_output(command).decode('utf-8').strip()
