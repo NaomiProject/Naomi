a2faf499e27c30d599f57c3c10ad29f60762e565
Aaron Chantrill <aaron.chantrill@dottywood.org>
2019-01-11 21:10:17 +0000

    Removed unnecessary error definition
    Removed the unnecessary definition for SystemError from Naomi.py.

--- Naomi.py
+++ Naomi.py
@@ -3,10 +3,6 @@
 import naomi
 import sys
 
-class SystemError(Exception):
-    def __init__(self,message):
-        self.message = message
-
 if(sys.version_info.major < 3):
     raise SystemError("This version of Naomi requires Python 3.5 or greater")
 
