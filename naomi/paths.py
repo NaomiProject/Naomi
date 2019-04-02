876a3081c3870ca4305bd96da60ffc173a849945
Austin Casteel <austin.casteel@gmail.com>
2019-04-02 15:12:40 -0400

    reverting path dir

--- naomi/paths.py
+++ naomi/paths.py
@@ -7,7 +7,7 @@ PKG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(PKG_PATH, "data")
 PLUGIN_PATH = os.path.normpath(os.path.join(PKG_PATH, os.pardir, "plugins"))
 
-CONFIG_PATH = os.path.expanduser(os.getenv('NAOMI_CONFIG', '~/.naomi/configs'))
+CONFIG_PATH = os.path.expanduser(os.getenv('NAOMI_CONFIG', '~/.naomi'))
 
 
 def config(*fname):
