450c7f16fa3bc21b49999421748c9b1220b62546
Ole Schulz-Trieglaff <ole.st@gmx.de>
2019-05-18 21:30:43 +0100

    Updating google-tts plugin. Small clarification to plugin.info for google-stt

--- plugins/tts/google-tts/google.py
+++ plugins/tts/google-tts/google.py
@@ -1,39 +1,57 @@
 import os
 import tempfile
-import gtts
+import logging
 from naomi import plugin
+from naomi import profile
+
+from google.cloud import texttospeech
 
 
 class GoogleTTSPlugin(plugin.TTSPlugin):
     """
-    Uses the Google TTS online translator
-    Requires pymad and gTTS to be available
+    Uses the Google TTS python API
+
+    https://cloud.google.com/text-to-speech/docs/reference/rpc/google.cloud.texttospeech.v1beta1#audioencoding
+
     """
 
     def __init__(self, *args, **kwargs):
         plugin.TTSPlugin.__init__(self, *args, **kwargs)
-        try:
-            orig_language = self.profile['language']
-        except:
-            orig_language = 'en-US'
 
-        language = orig_language.lower()
+        self._logger = logging.getLogger(__name__)
 
-        if language not in gtts.gTTS.LANGUAGES:
-            language = language.split('-')[0]
-
-        if language not in gtts.gTTS.LANGUAGES:
-            raise ValueError("Language '%s' ('%s') not supported" %
-                             (language, orig_language))
-
-        self.language = language
+        self.language = profile.get_profile_var(['language'], 'en-US')
+        self.client   = texttospeech.TextToSpeechClient()
+        # Build the voice request, select the language code and the ssml
+        # voice gender ("neutral")
+        self.voice =    texttospeech.types.VoiceSelectionParams(
+                        language_code=self.language,
+                        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+        # Select the type of audio file you want returned
+        self.audio_config = texttospeech.types.AudioConfig(
+                            audio_encoding=texttospeech.enums.AudioEncoding.LINEAR16)
 
     def say(self, phrase):
 
-        tts = gtts.gTTS(text=phrase, lang=self.language)
-        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as f:
-            tmpfile = f.name
-        tts.save(tmpfile)
-        data = self.mp3_to_wave(tmpfile)
-        os.remove(tmpfile)
-        return data
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.types.SynthesisInput(text=phrase)
+
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = self.client.synthesize_speech(synthesis_input, 
+                                                self.voice, 
+                                                self.audio_config
+                                                )
+
+        # The response's audio_content is binary.
+        #with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as f:
+        #    tmpfile = f.name
+        #    with open(tmpfile, 'wb') as out:
+        #        # Write the response to the output file.
+        #        out.write(response.audio_content)
+        #        print('Audio content written to file ' + tmpfile)
+
+            #data = self.mp3_to_wave(tmpfile)
+        #os.remove(tmpfile)
+        #return data
+        return response.audio_content
