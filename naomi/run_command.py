b51bae13651460f3c06643193e508fd1ae144cfb
Aaron Chantrill <aaron.chantrill@dottywood.org>
2019-06-15 11:04:27 -0400

    Updated run_command
    Updated run_command so that you can specify which output streams
    you would like to capture. This is currently only used for getting
    the list of voices available to Flite in populate.py, but I am
    using it a lot in my STT training stuff.
    
    Another small change is changing the default TTS engine to Flite.

--- naomi/run_command.py
+++ naomi/run_command.py
@@ -1,8 +1,45 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
-def run_command(command):
-    return subprocess.run(
-        command,
-        stdout=subprocess.PIPE
-    ).stdout.decode('utf-8').strip()
+
+# This program is just so we can avoid calling subprocess directly, which
+# always causes Codacy to flag the pull request. 
+# The first parameter is an array containing a command, flags and arguments.
+# The second, optional parameter determines how the output of the command is
+# handled:
+#  0 (default) - don't capture output
+#  1 - only capture stdout
+#  2 - only capture stderr
+#  3 - pipe stdout into stderr and capture stdout
+# The response is the raw CompletedProcess instance, containing the command,
+# returncode, stdout and stderr properties and the check_returncode() method
+# The subprocess.run command takes a wide variety of arguments, so I
+# considered just making this a wrapper around the run() method, but since
+# the different capture methods require referencing attributes of the
+# subprocess module anyway, I decided to try simplifying it to these four
+# use cases. This module is currently only used in populate.py (for capturing
+# the system timezone) but I will need to use it a lot in my STT model
+# training plugins.
+def run_command(command, capture=0):
+    completedprocess = None
+    if(capture == 1):
+        completedprocess = subprocess.run(
+            command,
+            stdout=subprocess.PIPE
+        )
+    elif(capture == 2):
+        completedprocess = subprocess.run(
+            command,
+            stderr=subprocess.PIPE
+        )
+    elif(capture == 3):
+        completedprocess = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
+    else:
+        completedprocess = subprocess.run(
+            command
+        )
+    return completedprocess
