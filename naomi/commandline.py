a584220be6b0eab2edb0b85b5dce8c248b03109d
Julian Liebig <je.liebig@web.de>
2019-10-21 16:48:50 +0200

    removed unused imports and added simple_password for email password

--- naomi/commandline.py
+++ naomi/commandline.py
@@ -3,15 +3,8 @@ from getpass import getpass
 from naomi import i18n
 from naomi import paths
 from naomi import profile
-from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import base64
 import re
 import sys
-import os
-import subprocess
 
 
 _ = None
--- naomi/populate.py
+++ naomi/populate.py
@@ -259,11 +259,11 @@ def get_email_info():
     # every time a new encryption method is added.
     if(profile.get_profile_password(["email", "address"])):
         prompt = _("What is your email password?") + ": "
-        if not (profile.get_profile_password(["email", "password"])):
+        if (profile.get_profile_password(["email", "password"])):
             prompt += interface.default_text(
                 _("(just press enter to keep current password)")
             ) + interface.default_prompt()
-        temp = interface.getpass(
+        temp = interface.simple_password(
             interface.format_prompt(
                 "?",
                 prompt
