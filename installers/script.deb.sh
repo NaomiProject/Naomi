c7596aca88eea6e9a3c946d9ce5880eb40992a56
aaronchantrill <aaron.chantrill@dottywood.org>
2020-07-03 11:05:37 +1200

    Bambocher pocketsphinx-python moved deps
    Bambocher's version of the pocketsphinx-python installer moves the pocketsphinx and sphinxbase subprojects into a subdirectory called deps. I missed this during testing. This fixes the path when building these projects.

--- installers/script.deb.sh
+++ installers/script.deb.sh
@@ -613,7 +613,7 @@ setup_wizard() {
         exit 1
       fi
     fi
-    cd pocketsphinx-python/sphinxbase
+    cd pocketsphinx-python/deps/sphinxbase
     ./autogen.sh
     make
     if [ $REQUIRE_AUTH -eq 1 ]; then
@@ -626,7 +626,7 @@ setup_wizard() {
     # Installing & Building pocketsphinx
     echo
     printf "${B_G}Building and installing pocketsphinx...${B_W}${NL}"
-    cd ~/.config/naomi/sources/pocketsphinx-python/pocketsphinx
+    cd ~/.config/naomi/sources/pocketsphinx-python/deps/pocketsphinx
     ./autogen.sh
     make
     if [ $REQUIRE_AUTH -eq 1 ]; then
