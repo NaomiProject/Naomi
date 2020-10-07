0c95859bdf5411fa419513d854988ae325dadc0e
Aaron Chantrill <aaron.chantrill@dottywood.org>
2020-10-06 23:04:39 -0400

    Fixed a few bugs in Naomi_tti
    Python appears to have changed some of the ways it works with
    strings since I worked on this last. The \b boundary regexp
    markers no longer match outside curly brackets, so I had to
    use re.finditer("{keyword}", text) instead of the original
    re.finditer("\b{keyword}\b", text).
    
    Also, I needed to add an additional "<END>" term to the template
    and text so they could match the end of the string.
    
    There's still a little cleanup to do, but it at least works again
    now.

--- plugins/tti/naomi_tti/naomi_tti.py
+++ plugins/tti/naomi_tti/naomi_tti.py
@@ -6,6 +6,7 @@ from jiwer import wer
 from naomi import paths
 from naomi import plugin
 from naomi import profile
+from pprint import pprint
 
 
 # Replace the nth occurrance of sub
@@ -15,11 +16,15 @@ def replacenth(search_for, replace_with, string, n):
     try:
         # print("Searching for: '{}' in '{}'".format(search_for, string))
         # pprint([m.start() for m in re.finditer(search_for, string)])
-        where = [m.start() for m in re.finditer("{}{}{}".format(r"\b", search_for, r"\b"), string)][n - 1]
+        where = [m.start() for m in re.finditer(search_for, string)][n - 1]
         before = string[:where]
+        # print("Before: {}".format(before))
         after = string[where:]
+        # print("After: {}".format(after))
         after = after.replace(search_for, replace_with, 1)
+        # print("After: {}".format(after))
         string = before + after
+        # print("String: {}".format(string))
     except IndexError:
         # print("IndexError {}".format(n))
         pass
@@ -170,7 +175,6 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
         return sorted(list(set(phrases)))
 
     def determine_intent(self, phrase):
-        # print(phrase)
         phrase = phrase.upper()
         score = {}
         allvariants = {phrase: {}}
@@ -318,13 +322,19 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
                 # substitutions in the template
                 if(possiblesubstitutions > 0):
                     for i in range(possiblesubstitutions):
-                        # print("i={}".format(i))
+                        # print("replacenth('{}','{}','{}',{})".format(
+                        #     '{}{}{}'.format('{', matchlist, '}'),
+                        #     word,
+                        #     currenttemplate,
+                        #     i + 1
+                        # ))
                         currenttemplate = replacenth(
                             '{}{}{}'.format('{', matchlist, '}'),
                             word,
                             currenttemplate,
                             i + 1
                         )
+                        # print("CurrentTemplate = {}".format(currenttemplate))
                         templates[currenttemplate] = wer(
                             currentvariant,
                             currenttemplate
@@ -339,7 +349,8 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
         # Now that we have a matching template, run through a list of all
         # substitutions in the template and see if there are any we have not
         # identified yet.
-        substitutions = re.findall('\{(.*?)\}', currenttemplate)
+        substitutions = re.findall(r'{(.*?)}', currenttemplate)
+        # print("Substitutions: {}".format(substitutions))
         if(substitutions):
             for substitution in substitutions:
                 subvar = "{}{}{}".format('{', substitution, '}')
@@ -352,8 +363,10 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
                 # print("Minimizing distance from '{}' to '{}' by substituting in '{}'".format(currentvariant,currenttemplate,subvar))
                 # print("Variant: {}".format(currentvariant))
                 variant = currentvariant.split()
+                variant.append("<END>")
                 # print("Template: {}".format(currenttemplate))
                 template = currenttemplate.split()
+                template.append("<END>")
                 n = len(variant) + 1
                 m = len(template) + 1
                 # print("Variant: '{}' length: {}".format(variant,n))
@@ -370,15 +383,11 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
                     a[0][j] = j
                 for i in range(1, n):
                     for j in range(1, m):
+                        # print("{},{} V: {} T: {}".format(i,j,variant[i-1],template[j-1]))
                         if(variant[i - 1] == template[j - 1]):
                             c = 0
                         else:
                             c = 1
-                        a[i][j] = min(
-                            a[i - 1][j] + 1,
-                            a[i][j - 1] + 1,
-                            a[i - 1][j - 1] + c
-                        )
                         a[i][j] = c
                 # pprint(a)
                 # examine the resulting list of matched words
@@ -412,8 +421,8 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
                         substitutedvariant = variant[:start]
                         substitutedvariant.append(subvar)
                         substitutedvariant.extend(variant[end:])
-                        break
                         # print("SubstitutedVariant: {}".format(substitutedvariant))
+                        break
                     elif(a[i + 1][s + 1] == 0):
                         # the next item is a match, so start working backward
                         k = i
@@ -437,8 +446,8 @@ class NaomiTTIPlugin(plugin.TTIPlugin):
                         substitutedvariant = variant[:start]
                         substitutedvariant.append(subvar)
                         substitutedvariant.extend(variant[end:])
-                        break
                         # print("SubstitutedVariant: {}".format(substitutedvariant))
+                        break
                 if(len(matched)):
                     # print("Match: '{}' to '{}'".format(substitution, matched))
                     try:
