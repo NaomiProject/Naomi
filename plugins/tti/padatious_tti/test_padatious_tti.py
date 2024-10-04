a95fd695f1b7441da46cf2601b5d4a2a6a8725e3
Aryan Yadav <141573833+aryan0931@users.noreply.github.com>
2024-10-04 18:34:11 +0530

    Remove padatious plugin from the Naomi repository (#420)

--- plugins/tti/padatious_tti/test_padatious_tti.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import unittest
-from naomi import testutils
-from . import padatious_tti
-
-
-class TestPadatiousTTIPlugin(unittest.TestCase):
-    def setUp(self):
-        self.plugin = testutils.get_plugin_instance(padatious_tti.PadatiousTTIPlugin)
